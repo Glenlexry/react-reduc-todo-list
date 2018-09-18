ad035b382e4b679fb28224da545964d7ed1897e1
glenlexry <glen@digitalsymphony.it>
2018-09-18 08:41:25 +0800

    Adding actions

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,29 +1,30 @@
 import React, { Component } from 'react';
 import AddTodo from './AddTodo'
 import TodoList from './TodoList'
+import { connect } from 'react-redux'
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      input: '',
-      todos: []
-    }
-  }
+  // constructor(props) {
+  //   super(props)
+  //
+  //   this.state = {
+  //     input: '',
+  //     todos: []
+  //   }
+  // }
 
   handleChange = (event) => {
-    this.setState({
-      input: event.target.value
-    })
+    // this.setState({
+    //   input: event.target.value
+    // })
   }
 
   handleSubmit = (event) => {
     event.preventDefault();
-    this.setState({
-      input: '',
-      todos: [...this.state.todos,this.state.input]
-    })
+    // this.setState({
+    //   input: '',
+    //   todos: [...this.state.todos,this.state.input]
+    // })
   }
 
 
@@ -33,13 +34,17 @@ class App extends Component {
       <AddTodo
       handleChange={this.handleChange}
       handleSubmit={this.handleSubmit}
-      value={this.state.input}
+      value={this.props.input}
       />
-      <TodoList todos={this.state.todos} />
+      <TodoList todos={this.props.todos} />
       </div>
     );
   }
-
 }
 
-export default App;
+const mapStatetoProps = state => ({
+  input: state.input,
+  todos: state.todos
+})
+
+export default connect(mapStatetoProps)(App);
--- src/index.js
+++ src/index.js
@@ -4,11 +4,36 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './components/App'
 
-// const store = createStore(todos, /* preloadedState, */
-//    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-// )
+const initialState = {
+  input: '',
+  todos: []
+}
+
+const addTodo = text => ({
+  type: 'ADD_TODO',
+  text
+})
+
+const todos = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+        return {
+          input: state.input,
+          todos: action.text
+        }
+      break;
+    default:
+      return state;
+  }
+}
+
+const store = createStore(todos, /* preloadedState, */
+   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 render(
-    <App/>,
+  <Provider store={store}>
+    <App/>
+  </Provider>,
   document.getElementById('root')
 )
